4494a8d8d96d1960f226b9c9abdbfb56a31934ce
Roman Baygildin <rbaygildin95@gmail.com>
2018-08-08 19:20:32 +0300

    Updated spec and added init.sql

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE devices (
+  id serial primary key,
+  name varchar(100) not null
+);
+
+CREATE TABLE gps_data (
+  id serial primary key,
+  latitude double precision not null,
+  logitude double precision not null,
+  time timestamp not null,
+  radius double precision not null,
+  device_id integer references devices not null
+);
